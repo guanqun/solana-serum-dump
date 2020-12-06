77f6577fdfa5c4d307c89898aea5464f5114b63e
Guanqun Lu <guanqun.lu@gmail.com>
2020-12-06 23:43:19 +0800

    pin down to the original serum swap state version and make it work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,68 @@
+#[macro_use]
+extern crate prettytable;
+use prettytable::{Cell, Row, Table};
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::pubkey::Pubkey;
+use spl_token_swap::{solana_program::program_pack::Pack, state::SwapInfo};
+use std::collections::HashMap;
+use std::str::FromStr;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let client = RpcClient::new_with_timeout(
+        "https://api.mainnet-beta.solana.com".into(),
+        Duration::from_secs(5),
+    );
+
+    let serum_program_id = Pubkey::from_str("9qvG1zUp8xF1Bi4m6UdRNby1BAAuaDrUxSpv4CmRRMjL")
+        .expect("valid serum program id");
+    let pool_accounts = client
+        .get_program_accounts(&serum_program_id)
+        .expect("can't get swap infos.");
+
+    // it's not possible to get back the corresponding token names from on-chain data,
+    // we'll need to consult it from third-party crate. I wrote a `spl-token-names` crate which does exactly this.
+    let token_maps: HashMap<Pubkey, String> = spl_token_names::TOKENS
+        .iter()
+        .map(|info| {
+            let pubkey = Pubkey::from_str(info.mint_address).expect("invalid pubkey");
+            let name = String::from(info.token_symbol);
+            (pubkey, name)
+        })
+        .collect();
+
+    // need to get the decimals so that we can output the right number.
+
+    // output the real name if we can find it, otherwise, dump the raw pubkey
+    // the format is as follows:
+    // ?? can we something like tabular crate?
+    // one line is one pool.
+    // pool pub key | token-a(SRM) | balance-of-a | token-b(USDT) | balance-of-b |
+    let mut table = Table::new();
+    table.add_row(row![
+        "Pool",
+        "Token A Pubkey",
+        //"Balance A",
+        "Token B Pubkey",
+        //"Balance B",
+        //"Curve"
+    ]);
+
+    for pool in &pool_accounts {
+        let mut cells = vec![];
+
+        // pool address
+        cells.push(Cell::new(format!("{}", pool.0).as_str()));
+
+        // parse swap data
+        let info = SwapInfo::unpack_from_slice(&pool.1.data).expect("invalid swap info");
+        cells.push(Cell::new(format!("{}", info.token_a).as_str()));
+        cells.push(Cell::new(format!("{}", info.token_b).as_str()));
+
+        table.add_row(Row::new(cells));
+    }
+
+    table.printstd();
+
+    // then we can enter an interactive mode where we can send out the swap instruction, that'll be quite cool.
 }
